3362f4cb395700b30d5cbdbff94d87c9a1ffcec4
NehaBombatkar <nehabombatkar@gmail.com>
2024-04-03 17:21:54 +0530

    selenium framweork

--- src/main/java/com/thetestingacademy/pages/PageFactory/DashboardPage_PF.java
+++ src/main/java/com/thetestingacademy/pages/PageFactory/DashboardPage_PF.java
@@ -1,4 +1,1 @@
 package com.thetestingacademy.pages.PageFactory;
-
-public class DashboardPage_PF {
-}
---
+++ src/test/java/com/thetestingacademy/vwo/LambdaTest/LoginTestRemote.java
@@ -0,0 +1,5 @@
+package com.thetestingacademy.vwo.LambdaTest;
+
+public class LoginTestRemote {
+    // this test case will run on Lambda test
+}
